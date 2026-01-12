66bf6aa7d01ab77b7594a2bdd1c7c513a587d703
Omar <omar.garcia@morsa.com.mx>
2026-01-12 13:10:58 -0600

    Creando ENDPOINT para OBTENER PROYECT por ID

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -28,4 +28,26 @@ export class ProjectController {
 
     res.send('Todos los proyectos');
   };
+
+  static getProjectById = async (req: Request, res: Response) => {
+    // console.log(req.params);
+    const { id } = req.params;
+    // console.log(id);
+
+    try {
+      const project = await Project.findById(id);
+
+      if (!project) {
+        const error = new Error('Proyecto NO encontrado...');
+
+        return res.status(404).json({ error: error.message });
+      }
+
+      res.json(project);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // res.send('Todos los proyectos');
+  };
 }
