cdd26acb329e35039f5781e6bcfebff262a17fa9
Omar <omar.garcia@morsa.com.mx>
2026-01-12 12:40:53 -0600

    Agregando VALIDACION

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,24 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 // En POSTMAN: http://localhost:4000/api/projects
-router.post('/', ProjectController.createProject);
-router.get('/', ProjectController.getAllProjects);
+router.post(
+  "/",
+  body("projectName")
+    .notEmpty()
+    .withMessage("El Nombre del Proyecto es Obligatorio"),
+  body("clientName")
+    .notEmpty()
+    .withMessage("El Nombre del Cliente es Obligatorio"),
+  body("description")
+    .notEmpty()
+    .withMessage("La Descripción es Obligatoria"),
+  handleInputErrors,
+  ProjectController.createProject
+);
+router.get("/", ProjectController.getAllProjects);
 
 export default router;
