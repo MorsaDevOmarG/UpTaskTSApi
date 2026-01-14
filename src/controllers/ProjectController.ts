d9d5aacb419b707487428ddda8b40de17e16577f
Omar <omar.garcia@morsa.com.mx>
2026-01-13 18:15:45 -0600

    Modificando metodo UPDATE de PROJECT

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -56,13 +56,18 @@ export class ProjectController {
     const { id } = req.params;
 
     try {
-      const project = await Project.findByIdAndUpdate(id, req.body);
+      // const project = await Project.findByIdAndUpdate(id, req.body);
+      const project = await Project.findById(id);
 
       if (!project) {
         const error = new Error("Proyecto NO encontrado...");
 
         return res.status(404).json({ error: error.message });
       }
+
+      project.projectName = req.body.projectName;
+      project.clientName = req.body.clientName;
+      project.description = req.body.description;
 
       await project.save();
 
