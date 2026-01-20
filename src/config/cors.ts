4e7a31cc3bff3d604e4e2494c3de4eb3744b8335
Omar <omar.garcia@morsa.com.mx>
2026-01-19 23:15:43 -0600

    Configurando CORS

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,11 +3,16 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     console.log(origin);
+    // console.log(process.argv);
     
     const whiteList = [process.env.FRONTEND_URL];
 
-    // if (whiteList.includes(origin)) {
-    if (!origin || whiteList.includes(origin)) {
+    if (process.argv[2] === '--api') {
+      whiteList.push(undefined);
+    }
+
+    if (whiteList.includes(origin)) {
+    // if (!origin || whiteList.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Error de CORS"));
