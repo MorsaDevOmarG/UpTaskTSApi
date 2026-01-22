fe2f11b288643bf8b4795caeb92e7a409d36c412
Omar <omar.garcia@morsa.com.mx>
2026-01-22 12:33:19 -0600

    DEfinir y HASHEAR el NUEVO PASSOWORD

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
@@ -10,78 +10,78 @@ const router = Router();
 // });
 
 router.post(
-  '/create-account',
-  body('name')
-    .notEmpty()
-    .withMessage('El Nombre no puede ir vacío'),
-  body('password')
+  "/create-account",
+  body("name").notEmpty().withMessage("El Nombre no puede ir vacío"),
+  body("password")
     .isLength({
-      min: 8
+      min: 8,
     })
-    .withMessage('El Password es muy corto, minímo 8 carácteres'),
-  body('password_confirmation')
-    .custom(
-      (value, { req }) => {
-        if (value !== req.body.password) {
-          throw new Error('Los Password no son iguales')
-        }
+    .withMessage("El Password es muy corto, minímo 8 carácteres"),
+  body("password_confirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Los Password no son iguales");
+    }
 
-        return true;
-      }
-    ),
-  body('email')
-    .isEmail()
-    .withMessage('El E-mail no es válido'),
+    return true;
+  }),
+  body("email").isEmail().withMessage("El E-mail no es válido"),
   handleInputErrors,
-  AuthController.createAccount
+  AuthController.createAccount,
 );
 
 router.post(
-  '/confirm-account',
-  body('token')
-    .notEmpty()
-    .withMessage('El Token no puede ir vacío'),
+  "/confirm-account",
+  body("token").notEmpty().withMessage("El Token no puede ir vacío"),
   handleInputErrors,
-  AuthController.confirmAccount
+  AuthController.confirmAccount,
 );
 
 router.post(
   "/login",
-  body("email")
-    .isEmail()
-    .withMessage("El E-mail no es válido"),
-  body("password")
-    .notEmpty()
-    .withMessage("El Password no puede ir vacío"),
+  body("email").isEmail().withMessage("El E-mail no es válido"),
+  body("password").notEmpty().withMessage("El Password no puede ir vacío"),
   handleInputErrors,
   AuthController.login,
 );
 
 router.post(
   "/request-code",
-  body("email")
-    .isEmail()
-    .withMessage("El E-mail no es válido"),
+  body("email").isEmail().withMessage("El E-mail no es válido"),
   handleInputErrors,
   AuthController.requestConfirmationCode,
 );
 
 router.post(
   "/forgot-password",
-  body("email")
-    .isEmail()
-    .withMessage("El E-mail no es válido"),
+  body("email").isEmail().withMessage("El E-mail no es válido"),
   handleInputErrors,
   AuthController.forgotPassword,
 );
 
 router.post(
   "/validate-token",
-  body("token")
-    .notEmpty()
-    .withMessage("El Token no puede ir vacío"),
+  body("token").notEmpty().withMessage("El Token no puede ir vacío"),
   handleInputErrors,
   AuthController.validateToken,
+);
+
+router.post(
+  "/update-password/:token",
+  param("token").isNumeric().withMessage("Token no válido"),
+  body("password")
+    .isLength({
+      min: 8,
+    })
+    .withMessage("El Password es muy corto, minímo 8 carácteres"),
+  body("password_confirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Los Password no son iguales");
+    }
+
+    return true;
+  }),
+  handleInputErrors,
+  AuthController.updatePasswordWithToken,
 );
 
 export default router;
