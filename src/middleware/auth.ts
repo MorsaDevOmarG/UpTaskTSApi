c2c9a073448efb6734bc0f614a22373bf54077e6
Omar <omar.garcia@morsa.com.mx>
2026-01-23 11:17:11 -0600

    Finalizando el MIDDLEWARE

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,14 @@
 import { Response, Request, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import User from '../models/User';
+import User, { IUser } from '../models/User';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser
+    }
+  }
+};
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
   // console.log(req.headers.authorization);
@@ -23,8 +31,15 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
     // console.log(decoded);
 
     if (typeof decoded === 'object' && decoded.id) {
-      const user = await User.findById(decoded.id);
-      console.log(user);
+      const user = await User.findById(decoded.id).select('_id name email');
+      // console.log(user);
+
+      if (user) {
+        req.user = user;
+      } else {
+        res.status(500).json({ error: "Token No Válido" });
+      }
+
     }
 
   } catch (error) {
