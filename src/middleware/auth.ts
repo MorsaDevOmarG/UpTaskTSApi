37305754d4b5bede70cc991ee2f6ba42823d0187
Omar <omar.garcia@morsa.com.mx>
2026-01-23 11:25:39 -0600

    identando  MIDDLEWARE

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,22 +1,26 @@
-import { Response, Request, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import User, { IUser } from '../models/User';
+import { Response, Request, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User, { IUser } from "../models/User";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: IUser
+      user?: IUser;
     }
   }
-};
+}
 
-export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+export const authenticate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   // console.log(req.headers.authorization);
 
   const bearer = req.headers.authorization;
 
   if (!bearer) {
-    const error = new Error('No Autorizado');
+    const error = new Error("No Autorizado");
 
     return res.status(401).json({ error: error.message });
   }
@@ -30,21 +34,18 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     // console.log(decoded);
 
-    if (typeof decoded === 'object' && decoded.id) {
-      const user = await User.findById(decoded.id).select('_id name email');
+    if (typeof decoded === "object" && decoded.id) {
+      const user = await User.findById(decoded.id).select("_id name email");
       // console.log(user);
 
       if (user) {
         req.user = user;
+        next();
       } else {
         res.status(500).json({ error: "Token No Válido" });
       }
-
     }
-
   } catch (error) {
-    res.status(500).json({ error: 'Token No Válido' });
+    res.status(500).json({ error: "Token No Válido" });
   }
-
-  next();
 };
