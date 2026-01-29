d1ad911743dad6a47d7d04fed86ffc0ed8df604f
Omar <omar.garcia@morsa.com.mx>
2026-01-28 23:56:15 -0600

    Permitiendo a los COLABORADORES ver los PROYECTOS

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -32,7 +32,10 @@ export class ProjectController {
       const projects = await Project.find({
         $or: [
           {
-            manager: { $in: req.user._id },
+            manager: req.user._id ,
+          },
+          {
+            team:  req.user._id ,
           },
         ],
       });
@@ -60,7 +63,7 @@ export class ProjectController {
         return res.status(404).json({ error: error.message });
       }
 
-      if (project.manager.toString() !== req.user._id.toString()) {
+      if (project.manager.toString() !== req.user._id.toString() && !project.team.includes(req.user._id)) {
         const error = new Error("Acción no válida...");
 
         return res.status(404).json({ error: error.message });
